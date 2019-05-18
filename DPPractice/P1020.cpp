f05c9aed91d9e8d678679b74df7622c4324e0fbb
AI1379 <Kc20060529@163.com>
2019-05-18 17:54:35 +0800

    整理了一下0-1背包的程序

---
+++ DPPractice/P1020.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int h[100010],f[100010];
+int n,x;
+int main(){
+  int i,j,ans1=0,ans2=0;
+  cin>>n;
+  for(i=1;i<=n;i++)cin>>h[i];
+  //while(cin>>h[++n])continue;
+  //while(cin>>x){
+  //  n++;
+  //  h[n]=x;
+  //}
+  for(i=1;i<=n;i++){
+    for(j=1;j<i;j++){
+      if(h[j]>=h[i]){
+        f[i]=max(f[i],f[j]);
+      }
+    }
+    f[i]++;
+  }
+  for(i=1;i<=n;i++)ans1=max(ans1,f[i]);
+  for(i=1;i<=n;i++){
+    bool flag=false;
+    int wh=0,nowh=1<<30;
+    for(j=1;j<=ans2;j++)
+      if(f[j]>=h[i]&&f[j]<nowh)
+        nowh=f[j],wh=j;
+    if(wh)f[wh]=h[i];
+    else f[++ans2]=h[i];
+  }
+  cout<<ans1<<endl<<ans2;
+  return 0;
+}
