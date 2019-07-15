bd6106056eb6436484a51e217e7c87f76a118fd4
AI1379 <Kc20060529@163.com>
2019-07-15 14:05:12 +0800

    Change

--- MonthlyCompetition/luoguJulyCompetition/P5463.cpp
+++ MonthlyCompetition/luoguJulyCompetition/P5463.cpp
@@ -1,16 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 const int MAXL=1000000;
-int fish[MAXL],cnt;
+unsigned long long fish[MAXL],cnt;
 int main(){
-	int n,i,j,k,ans=0;
-	vector<int>f[3];
+	unsigned long long n,i,j,k,ans=0;
+	vector<unsigned long long>f[2];
 	cin>>n;
 	f[0].resize(n+1);
 	f[1].resize(n+1);
-	f[2].resize(n+1);
 	for(i=0;i<n;i++) cin>>fish[i];
-	for(i=0;i<n;i++) f[1][i]=0;
+	for(i=0;i<n;i++) f[0][i]=0;
 	for(k=2;k<=n;k++){
 		for(i=0;i<n-k+1;i++){
 			cnt=0;
@@ -18,18 +17,10 @@ int main(){
 				if(fish[j]>fish[i+k-1])
 					cnt++;
 			}
-			f[2][i]=f[1][i]+cnt;
-			ans+=f[2][i];
+			f[(k+1)%2][i]=f[k%2][i]+cnt;
+			ans+=f[(k+1)%2][i];
 		}
-		for(i=0;i<n;i++) f[1][i]=f[2][i];
 	}
-//	for(i=1;i<=n;i++){
-//		for(j=0;j<n;j++){
-//			ans+=f[i][j];
-//			cout<<f[i][j]<<' ';
-//		} 
-//		cout<<endl;
-//	} 
 	cout<<ans;
 	system("pause");
 	return 0;
--- MonthlyCompetition/luoguJulyCompetition/test.cpp
+++ MonthlyCompetition/luoguJulyCompetition/test.cpp
@@ -1,17 +1,73 @@
-#include<bits/stdc++.h>
+#pragma GCC optimize(5)//O5优化
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<cmath>
+#include<cstring>
+#define LOLO long long
 using namespace std;
-int main(){
-    int n;
-    cin>>n;
-    switch (n)
+LOLO AA,Ta,Ans,GG[1005],A[1000005],N,B[1000005],C[1000005];
+LOLO L_B(LOLO x){return x&(-x);}
+inline LOLO Read()
+{
+    LOLO x=0,f=1;
+    char ch=getchar();
+    while(ch<'0'||ch>'9')
     {
-    case 1:
-        cout<<"0 1\n1 1";
-        break;
-    
-    default:
-        break;
+        if(ch=='-')f=-1;
+        ch=getchar();
     }
+    while(ch>='0'&&ch<='9')
+    {
+        x=x*10+ch-'0';
+        ch=getchar();
+    }
+    return x*f;
+}
+inline void Write(LOLO x)
+{
+    if(x<0)
+    {
+        putchar('-');
+        x=-x;
+    }
+    if(x>9)
+    {
+        Write(x/10);
+    }
+    putchar(x%10+'0');
+}
+void Add(LOLO X,LOLO Num)
+{
+    for(LOLO i=X;i<=N;i+=L_B(i))
+    {
+        C[i]+=Num;
+    }
+}
+LOLO Ask(LOLO x)
+{
+    LOLO Ans=0;
+    for(LOLO i=x;i;i-=L_B(i))
+    {
+        Ans+=C[i];
+    }
+    return Ans;
+}
+int main()
+{
+    N=Read();
+    for(LOLO i=1;i<=N;i++)A[i]=Read(),B[i]=A[i];
+    sort(B+1,B+1+N);
+    LOLO Num=unique(B+1,B+N+1)-B-1;
+    for(LOLO i=1;i<=N;i++)A[i]=lower_bound(B+1,B+1+Num,A[i])-B;
+    for(LOLO i=N;i>=1;i--)//枚举区间长度 
+    {
+        LOLO Num=0;
+        Num=(Ask(A[i]-1))*i;
+        Add(A[i],N-i+1);
+        Ans+=Num;
+    }
+    Write(Ans);
     system("pause");
     return 0;
 }
