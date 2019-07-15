6e58c04893c8fc5c9739de5f0f6c369c1d6d5593
AI1379 <Kc20060529@163.com>
2019-07-15 09:32:07 +0800

    Change

---
+++ MonthlyCompetition/luoguJulyCompetition/P5463.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int MAXL=1000000;
+int fish[MAXL],cnt;
+int main(){
+	int n,i,j,k,ans=0;
+	vector<vector<int> >f;
+	cin>>n;
+	f.resize(n+1);
+	for(i=0;i<=n;i++)
+		f[i].resize(n+1);
+	for(i=0;i<n;i++) cin>>fish[i];
+	for(i=0;i<n;i++)f[1][i]=0;
+	for(k=2;k<=n;k++){
+		for(i=0;i<n-k+1;i++){
+			cnt=0;
+			for(j=i;j<i+k-1;j++){
+				if(fish[j]>fish[i+k-1])
+					cnt++;
+			}
+			f[k][i]=f[k-1][i]+cnt;
+		}
+	}
+	for(i=1;i<=n;i++){
+		for(j=0;j<n;j++){
+			ans+=f[i][j];
+			//cout<<f[i][j]<<' ';
+		} 
+		//cout<<endl;
+	} 
+	cout<<ans;
+//	system("pause");
+	return 0;
+}
