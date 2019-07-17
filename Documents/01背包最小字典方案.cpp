4f7b84c9638647751b5d52e08f5da3555536863c
AI1379 <Kc20060529@163.com>
2019-07-17 15:20:49 +0800

    Change

---
+++ Documents/01背包最小字典方案.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int c[maxn],v[maxn];
+int main()
+{
+    int n,V;
+    cin>>n>>V;
+   //先逆序，从后向前，这样有利于输出时是按照最小字典序来
+    for(int i=n;i>=1;i--)
+        cin>>c[i]>>v[i];
+    for(int i=1;i<=n;i++)
+        for(int j=V;j>=c[i];j--)
+            f[j]=max(f[j],f[j-c[i]]+v[i]);
+    for(int i=n;i>=1;i--)
+        if(f[V]==f[V-c[i]]+v[i])//两者相同以选序号最小的情况为主
+        {
+            cout<<n-i+1<<' ';
+            V-=c[i];
+        }
+	return 0;
+}
+
