d30a0a6f1102ccafb1eff18af87149035e8dcbca
AI1379 <Kc20060529@163.com>
2019-07-24 22:05:37 +0800

    Change

--- SummerVacation/20190724/Mine/love.cpp
+++ SummerVacation/20190724/Mine/love.cpp
@@ -1,24 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
+long long lg2(long long n){
+	int x=n,ans=0;
+	while(x>0){
+		x=x>>1;
+		ans++;
+	}
+	return ans;
+}
+long long non(long long n){
+	if(n<2)return 0;
+	else if(n==2||n==3)return 1;
+	long long x=lg2(n),ans,i,k;
+	ans=(x-2)*(x-3)/2;
+	i=1<<(x-3);
+	k=(1<<x)-(1<<(x-2))-1;
+	while(k<=n){
+		ans++;
+		i=i>>1;
+		k+=i;
+	}
+	return ans;
+}
 int main(){
-	long long l,r,t,i,k,x,ans,num;
+	long long l,r,t,i,k,x,ans,num,n;
 	cin>>t;
 	for(k=0;k<t;k++){
 		cin>>l>>r;
-		ans=0;
-		for(i=l;i<=r;i++){
-			num=0;
-			x=i;
-			while(x>0){
-				if(x%2==0)
-					num++;
-				x=x>>1;
-			}
-			if(num==1){
-				ans++;
-			}
-		}
-		cout<<ans<<endl;
+		cout<<(non(r)-non(l-1))<<endl;
 	}
 	system("pause");
 	return 0;
