9f835ea480192e895b0766ae1bb58ea57fd9fd02
AI1379 <Kc20060529@163.com>
2019-08-02 20:10:43 +0800

    Change

--- DPPractice/0-1 Package/P1064(2).cpp
+++ DPPractice/0-1 Package/P1064(2).cpp
@@ -2,9 +2,10 @@
 using namespace std;
 const int maxn=32010;
 const int maxm=4900;
-int f[maxn],w[maxn],c[maxn],tmpw[maxm],tmpc[maxm],ai[maxm],n,m,tmpm;
+int f[maxm][maxn],w[maxn],c[maxn],tmpw[maxm],tmpc[maxm],ai[maxm],n,m,tmpm;
+bool vis[maxm][maxm];
 int main(){
-	int i,j,p,q;
+	int i,j,k,p,q,tmp,tmp2;
 	cin>>n>>m;
 	tmpm=m;
 	memset(ai,0,sizeof(ai));
@@ -20,6 +21,8 @@ int main(){
 				ai[q]=j;
 				w[j]=tmpw[q]+tmpw[i];
 				c[j]=tmpc[q]+tmpc[i];
+				vis[q][j]=true;
+				vis[j][q]=true;
 			}
 			else{
 				w[j]=tmpw[q]+tmpw[i];
@@ -28,15 +31,33 @@ int main(){
 				w[j]=w[ai[q]]+tmpw[i];
 				c[j]=c[ai[q]]+tmpc[i];
 				tmpm++;
+				vis[q][j]=true;
+				vis[j][q]=true;
+				vis[q][j-1]=true;
+				vis[j-1][q]=true;
+				vis[ai[q]][j]=true;
+				vis[j][ai[q]]=true;
 			}
 		}
 	}
 	m=tmpm;
 	for(i=1;i<=m;i++){
 		for(j=n;j>=c[i];j--){
-			f[j]=max(f[j],f[j-c[i]]+w[i]);
+			tmp=0;
+			tmp2=0;
+			for(k=0;k<i;k++){
+				if(vis[k][i]){
+					tmp2+=w[k];
+					tmp+=c[k];
+					f[i][j]=max(f[i-1][j],f[i-1][j+tmp]-tmp2+w[i]);
+				}
+				else{
+					f[i][j]=max(f[i-1][j],f[i-1][j-c[i]]+w[i]);
+				}
+			}
 		}
 	}
-	cout<<f[n];
+	cout<<f[m][n];
+	system("pause");
 	return 0;
 }
