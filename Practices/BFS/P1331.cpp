6d5985c300b4b51a4863b5f993e65d3452e91352
AI1379 <Kc20060529@163.com>
2020-05-23 11:38:21 +0800

    P1443 AC P1331 91 loj10000 AC

--- Practices/BFS/P1331.cpp
+++ Practices/BFS/P1331.cpp
@@ -1,19 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int dx[] = {1, -1, 0, 0};
-const int dy[] = {0, 0, 1, -1};
+const int dx[] = {-1, 1, 0, 0};
+const int dy[] = {0, 0, -1, 1};
 struct pos
 {
     int x, y;
 };
+bool mp[1000][1000];
+bool vis[1000][1000];
 int r, c, ans = 0;
-bool mp[1000][1000], vis[1000][1000];
-queue<pos> q;
+bool bfs(pos in)
+{
+    int k, minx, miny, maxx = 0, maxy = 0, s = 1;
+    pos tmp, head;
+    bool res = true;
+    queue<pos> q;
+    q.push(in);
+    minx = in.x;
+    miny = in.y;
+    vis[in.x][in.y] = true;
+    while (!q.empty())
+    {
+        head = q.front();
+        for (k = 0; k < 4; k++)
+        {
+            if (0 <= head.x + dx[k] && head.x + dx[k] < c && 0 <= head.y + dy[k] && head.y + dy[k] < r)
+            {
+                if (!vis[head.x + dx[k]][head.y + dy[k]] && mp[head.x + dx[k]][head.y + dy[k]])
+                {
+                    tmp.x = head.x + dx[k];
+                    tmp.y = head.y + dy[k];
+                    q.push(tmp);
+                    vis[tmp.x][tmp.y] = true;
+                    if (tmp.x < minx || tmp.y < miny)
+                    {
+                        res = false;
+                    }
+                    s++;
+                    maxx = max(tmp.x, maxx);
+                    maxy = max(tmp.y, maxy);
+                }
+            }
+        }
+        q.pop();
+    }
+    if (s == 1)
+    {
+        maxx = in.x;
+        maxy = in.y;
+    }
+    if ((maxx - minx + 1) * (maxy - miny + 1) != s)
+        res = false;
+    return res;
+}
 int main()
 {
+    memset(vis, false, sizeof(vis));
     string s;
-    int i, j, k, cnt;
-    memset(vis, 0, sizeof(vis));
+    pos tmp;
+    int i, j;
     cin >> r >> c;
     for (i = 0; i < r; i++)
     {
@@ -30,54 +75,23 @@ int main()
     {
         for (j = 0; j < c; j++)
         {
-            if (vis[i][j])
-                continue;
-            if (mp[i][j])
+            if (mp[i][j] && !vis[i][j])
             {
-                pos tmp, head, lu, rd;
-                q.push(tmp);
-                cnt = 1;
-                lu = tmp;
-                rd = tmp;
-                vis[i][j] = true;
-                while (!q.empty())
+                tmp.x = i;
+                tmp.y = j;
+                if (bfs(tmp))
                 {
-                    head = q.front();
-                    for (k = 0; k < 4; k++)
-                    {
-                        if (0 <= head.x + dx[k] && head.y + dx[k] < r && 0 <= head.y + dy[k] && head.y + dy[k] < c)
-                        {
-                            if (!vis[head.x + dx[k]][head.y + dy[k]] && mp[head.x + dx[k]][head.y + dy[k]])
-                            {
-                                tmp.x = head.x + dx[k];
-                                tmp.y = head.y + dy[k];
-                                q.push(tmp);
-                                vis[tmp.x][tmp.y] = true;
-                                lu.x = min(lu.x, tmp.x);
-                                lu.y = min(lu.y, tmp.y);
-                                rd.x = max(rd.x, tmp.x);
-                                rd.y = max(rd.y, tmp.y);
-                                cnt++;
-                            }
-                        }
-                    }
-                    q.pop();
-                }
-                if (cnt != (rd.x - lu.x) * (rd.y - lu.y))
-                {
-                    printf("Bad placement.");
-                    return 0;
+                    ans++;
                 }
                 else
                 {
-                    ans++;
+                    cout << "Bad placement.";
+                    return 0;
                 }
             }
         }
     }
     printf("There are %d ships.", ans);
-#ifdef DEBUG
-    getchar();
-#endif // DEBUG
+    system("pause");
     return 0;
 }
--- README.md
+++ README.md
@@ -5,10 +5,6 @@
 + 其他资料
 ## 题库
 + MonthlyCompetition文件夹包括按时间顺序整理的月赛题库及附带文件
-+ 以"Practice"结尾的所有文件夹下的文件，来自各大OJ，列表如下：
-  * [洛谷]("www.luogu.org")    文件以P开头，后面是题目编号
-  * uVA    文件以uva开头
-## 模板
-Templates文件夹下的文件，一般以头文件的形式保存
++ Practice文件夹下的文件，来自各大OJ
 ## 其他资料
 Documents文件夹下有学习资料和笔记。
