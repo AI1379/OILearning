b16f8fd367b7b2e61b5216d49524c1f1f03d267b
AI1379 <Kc20060529@163.com>
2020-05-24 14:34:32 +0800

    Format loj10000

--- Practices/Greety/loj10000.cpp
+++ Practices/Greety/loj10000.cpp
@@ -1,27 +1,33 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-struct activity{
-    int s,f;
+struct activity
+{
+    int s, f;
 };
 int n;
 activity E[1000];
-bool cmp(activity A,activity B){
-    return A.f<=B.f;
+bool cmp(activity A, activity B)
+{
+    return A.f <= B.f;
 }
-int main(){
-    int i,latest=0,ans=0;
-    cin>>n;
-    for(i=0;i<n;i++){
-        cin>>E[i].s>>E[i].f;
+int main()
+{
+    int i, latest = 0, ans = 0;
+    cin >> n;
+    for (i = 0; i < n; i++)
+    {
+        cin >> E[i].s >> E[i].f;
     }
-    sort(E,E+n-1,cmp);
-    for(i=0;i<n;i++){
-        if(E[i].s>=latest){
-            latest=E[i].f;
+    sort(E, E + n - 1, cmp);
+    for (i = 0; i < n; i++)
+    {
+        if (E[i].s >= latest)
+        {
+            latest = E[i].f;
             ans++;
         }
     }
-    cout<<ans;
+    cout << ans;
     system("pause");
     return 0;
 }
